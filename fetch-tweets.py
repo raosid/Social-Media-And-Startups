918b8b3b688c994f2e12cce88361dd335579df01
Siddharth Rao <sidgrao@gmail.com>
2015-10-12 03:13:51 -0700

    Connected to the twitter API and being able to access profiles

--- fetch-tweets.py
+++ fetch-tweets.py
@@ -6,3 +6,11 @@
 """
 
 import config
+import tweepy #http://tweepy.readthedocs.org/en/v3.4.0/
+
+# Testing out some basic code out for a single user.
+authentication = tweepy.OAuthHandler(config.API_KEY, config.API_SECRET)
+api = tweepy.API(authentication)
+
+gustoHQ = api.get_user("GustoHQ")
+print gustoHQ.followers_count
