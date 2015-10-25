40e7979c1d3d3d35cc9974dd2d24632794c04bb3
Siddharth Rao <sidgrao@gmail.com>
2015-10-24 18:54:38 -0700

    made changes to the script. In testing mode now

--- fetch-tweets.py
+++ fetch-tweets.py
@@ -2,8 +2,7 @@
     Authors: Benjamin Hallen, Alex Murray, Siddharth Rao
     Date: 10/11/2015
 
-    Fetches public tweets from certain users. Retrieves users
-    stored in a csv file.
+    Fetches tweets for companies and their CEOs.
 
     Initially the tweets are stored in .txt files (Just testing some stuff.)
     Note: Only fetches 200 tweets per user. Decreased the limit for testing
@@ -14,8 +13,8 @@ import config
 import tweepy # http://tweepy.readthedocs.org/en/v3.4.0/
 import csv # in order to read the csv file containing the list of accounts
 import os
+import json
 
-# Testing out some basic code out for a single user.
 def get_tweets_for_csv_accounts(api):
     # Fetches 200 tweets for each user in the csv file and
     # stores them in a text file named after the company.
@@ -25,31 +24,25 @@ def get_tweets_for_csv_accounts(api):
     counter = 0
     dict_of_tweets = {}
     for row in csv_file: # fetching the tweets
-        if counter > 0: # The first row contains the heading (Trash/Throwaway)
-            test = get_tweets_for_handler(row[0], row[1], api, dict_of_tweets) #1 is column of twitter handle
+        if counter > 1 and counter < 3: # The first row contains the heading (Trash/Throwaway)
+            get_tweets_for_handler(row[0], row[1], api, dict_of_tweets) #1 is column of twitter handle
         counter = counter + 1
 
-    #print dict_of_tweets
     # Saving the tweets in a text file.
-    save_tweets_to_text_files(dict_of_tweets)
+    # save_tweets_to_text_files(dict_of_tweets)
 
 
 def get_tweets_for_handler(company_name, handler, api, dict_of_tweets):
-    # Returns a list of tweets for the given handler. Limits the number
-    # of tweets to 200.
+    # Returns a list of tweets for the given handler.
     if len(handler) > 0:
         timeline_object = api.user_timeline(handler, count = 200) #limit is 200
 
         list_of_tweets = []
         for tweet in timeline_object:
-            #print "--------"
-            #print tweet.text
-            list_of_tweets.append(tweet.text.encode("utf-8"))
+            if tweet.id == 657614137376137216:
+                list_of_tweets.append(tweet)
+                dict_of_tweets[company_name] = list_of_tweets
 
-        dict_of_tweets[company_name] = list_of_tweets
-
-        return 1
-    return 0 # Returns an empty dict if there is no twitter profile of the user.
 
 
 
