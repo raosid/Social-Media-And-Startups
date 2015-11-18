189cafa3da00fb92d6941774692d2ea4eca868a4
Siddharth Rao <sidgrao@gmail.com>
2015-11-17 20:29:42 -0800

    Script works. Added doordash

--- angellist_get_founders.py
+++ angellist_get_founders.py
@@ -2,7 +2,8 @@
     Authors: Benjamin Hallen, Alex Murray, Siddharth Rao
     Date: 10/24/2015
 
-    Fetches information about startups in the United States.
+    Fetches information about founders of startups in
+    the United States of America given a startup name.
 """
 import config
 import requests
@@ -32,13 +33,30 @@ def fetch_id_of_startup_from_db(db, name_of_company):
 
 def fetch_founders_for_startup(startup_id):
     """
+        @params startup_id: The id of the startup
+        @return  A list of dictionaries (json objects), each
+        of which contains information about a founder of the startup
 
+        Extra Info on the process:
+            The AngelList API has one endpoint that
+            returns all the founders* in a company. But that
+            endpoint only returns basic information about
+            the founders.
+
+            Once we have access to the ids of the founders,
+            we use another endpoint to fetch all the information
+            about them.
+
+            *Some founders are not correctly mentioned as founders.
+            Still need to figure out what to do in that case.
+                1 option:
+                    Fetch all roles of that company.
     """
     params = {
         'access_token': config.CLIENT_TOKEN_AL,
         'v': 1,
-        'startup_id': startup_id, # Test case for Angel List
-        'role': 'founder'
+        'startup_id': startup_id,
+        'role': 'founder' # Remove this if want to fetch all of them
     }
     base_url = "https://api.angel.co/1/startup_roles"
     founder_info = requests.get(base_url, params)
@@ -49,6 +67,14 @@ def fetch_founders_for_startup(startup_id):
 
 
 def get_id_of_user(response):
+    """
+        @params response: The response text from startup_roles
+        endpoint from AngelList's API
+
+        @return Grabs the ids of the founders from the response
+        and returns them as a list. If none found, returns
+        an empty list
+    """
     res = []
     if "startup_roles" in response.json().keys():
         for role in response.json()["startup_roles"]:
@@ -57,8 +83,18 @@ def get_id_of_user(response):
 
 
 def fetch_user_info_from_api(list_of_ids, startup_id):
-    # returns a list of dictionaries (each containing)
-    # information about a founder
+    """
+        @params
+        list_of_ids: Contains the ids of founders in
+        startup_id: The id of the startup. This id is added
+        to the json object to quickly identify all founders
+        of a company given the company id.
+
+        @return
+        A dictionary of json objects, each of which
+        contains information about one founder.
+
+    """
     result = []
     for user_id in list_of_ids:
         params = {
@@ -75,6 +111,17 @@ def fetch_user_info_from_api(list_of_ids, startup_id):
 
 
 def save_founder_info_in_founders_db(list_of_dicts, db):
+    """
+        @params
+        list_of_dicts: A list of dictionaries, each of which
+        contains information for one founder of the company.
+        db: A reference to the mongodb database
+
+        PostCondition: For each of the founders in the list,
+        saves their information to the database.
+        If a document already exists in the database with the
+        same founder name, updates it.
+    """
     for founder in list_of_dicts:
         json_object = json.loads(dumps(founder))
         name_of_founder = json_object['name']
@@ -84,17 +131,30 @@ def save_founder_info_in_founders_db(list_of_dicts, db):
 
 
 def save_info_in_file(list_of_founders, name_of_company):
-        dirname = name_of_company
-        if not os.path.isdir("founders/" + dirname):
-            os.mkdir("founders/" + dirname)
+    """
+        @params
+        list_of_dicts: A list of dictionaries, each of which
+        contains information for one founder of the company.
+        name_of_company: The name of the company.
 
-        for founder in list_of_founders:
-            filename = founder['name'] + "_" + str(founder['id'])
-            path = "founders/%s/%s.json" %(dirname, filename)
-            outfile = open(path, 'w')
-            json_object = json.loads(dumps(founder))
-            print json_object["linkedin_url"]
-            json.dump(json_object, outfile)
+        PostCondition: Creates a folder for the company
+        and saves the information of the founder in a json
+        file in the folder.
+        The file name would look like:
+            <founder name>_<id on angellist>.json
+        If there's already a file for that founder, updates it.
+    """
+    dirname = name_of_company
+    if not os.path.isdir("founders/" + dirname):
+        os.mkdir("founders/" + dirname)
+
+    for founder in list_of_founders:
+        filename = founder['name'] + "_" + str(founder['id'])
+        path = "founders/%s/%s.json" %(dirname, filename)
+        outfile = open(path, 'w')
+        json_object = json.loads(dumps(founder))
+        print json_object["linkedin_url"]
+        json.dump(json_object, outfile)
 
 
 if __name__ == '__main__':
